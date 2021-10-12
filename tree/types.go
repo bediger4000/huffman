c13c3938b6d44f33f49f73ea45ad9282548b8600
Bruce Ediger <bediger8@gmail.com>
2021-10-11 20:05:21 -0600

    Node interface LeftChild, RightChild methods
    need these to be able to traverse the huffman-encoding
    tree so as to create dot-format output

--- tree/types.go
+++ tree/types.go
@@ -4,17 +4,19 @@ type Node interface {
 	Value() float64
 	IsNil() bool
 	String() string
+	LeftChild() Node
+	RightChild() Node
 }
 
 type Leaf struct {
-	Freq   float64
-	Char   rune
-	Parent *Interior
+	Freq float64
+	Char rune
+	// Parent *Interior
 }
 
 type Interior struct {
-	Freq   float64
-	Left   Node
-	Right  Node
-	Parent *Interior
+	Freq  float64
+	Left  Node
+	Right Node
+	// Parent *Interior
 }
