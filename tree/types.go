a3ccdc698073e72819190489f6aecc0d4d14b968
Bruce Ediger <bediger8@gmail.com>
2021-10-13 08:03:31 -0600

    go doc comments

--- tree/types.go
+++ tree/types.go
@@ -1,5 +1,10 @@
+// Package tree represents a Huffman-coding tree of symbols (Go runes)
+// and their frequencies. You can print a dot-format representation of
+// a tree on stdout.
 package tree
 
+// Node interface allows the tree code to not care if it
+// deals with a leaf or an interior node.
 type Node interface {
 	Value() float64
 	IsNil() bool
@@ -8,15 +13,17 @@ type Node interface {
 	RightChild() Node
 }
 
+// Leaf node in huffman-encoding tree. No children,
+// holds a symbol and its frequency
 type Leaf struct {
 	Freq float64
 	Char rune
-	// Parent *Interior
 }
 
+// Interior node in huffman-encoding tree. Has children,
+// and frequency of subtrees combined, but no children
 type Interior struct {
 	Freq  float64
 	Left  Node
 	Right Node
-	// Parent *Interior
 }
