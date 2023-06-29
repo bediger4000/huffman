d9714664d61d5ceed6615204938682d2011d23da
Bruce Ediger <bediger8@gmail.com>
2023-06-28 21:53:07 -0600

    fix usage error

--- heap/types.go
+++ heap/types.go
@@ -21,6 +21,6 @@ Thus the children of the node at position n would
 2n + 1 and 2n + 2 in a zero-based array.
 Computing the index of the parent node of n-th element is also
 straightforward.
-Similarly, for zero-based arrays, is the parent is
+Similarly, for zero-based arrays, the parent is
 located at position (n-1)/2 (floored).
 */
